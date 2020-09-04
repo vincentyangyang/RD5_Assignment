29179ca6575e9102924cf5b8c46dd9c3492eb444
vincentyangyang <vincent_yang@mail.chungyo.net>
2020-09-04 09:22:38 +0800

    demo

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 
 header("content-type:text/html; charset=utf-8");
 
-$db = new PDO("mysql:host=127.0.0.1;dbname=my_Bank", "root", "root");
+$db = new PDO("mysql:host=127.0.0.1;dbname=myBank", "root", "root");
 $db->exec("SET CHARACTER SET utf8");
 
 
